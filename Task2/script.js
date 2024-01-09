8792b38bcf999ed60420ff38cd4472f1edc49e8f
priyanshusingh <9415762705ps@gmail.com>
2024-01-09 17:44:28 +0530

    .

---
+++ Task2/script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // You can add logic for form submission here, e.g., sending an email or storing data.
+    alert('Form submitted successfully!');
+});
